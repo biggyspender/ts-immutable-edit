a91204dae4051575e2882a2eddc30073f7a2d1d6
Chris Sperry <chris.sperry@outthink.io>
2022-05-15 00:03:34 +0000

    make proxy handler own class

---
+++ src/immutable/edit/src/ImmutableProxyHandler.ts
@@ -0,0 +1,101 @@
+import { MATERIALIZE_PROXY } from './symbol/MATERIALIZE_PROXY';
+import { materializeProxy } from './materializeProxy';
+import { isMaterializable } from '../src/types/Materializable/guard/isMaterializable';
+import { makeCopyRef } from './makeCopyRef';
+import { CopyRef } from '../src/types/CopyRef';
+
+export class ImmutableProxyHandler<T extends object>
+  implements ProxyHandler<T>
+{
+  copyRef: CopyRef<T> | undefined;
+  changed = false;
+  materializedRef:
+    | {
+        value: T;
+        isCopy: boolean;
+      }
+    | undefined;
+  originalTarget: T;
+  createProxy: <T extends object>(v: T) => T;
+
+  constructor(originalTarget: T, createProxy: <T extends object>(v: T) => T) {
+    this.originalTarget = originalTarget;
+    this.createProxy=createProxy;
+  }
+  get(target: T, propKey: PropertyKey, receiver: any) {
+    switch (propKey) {
+      case MATERIALIZE_PROXY: {
+        return () => {
+          if (this.materializedRef) {
+            return this.materializedRef;
+          }
+          const val = materializeProxy(
+            this.originalTarget,
+            this.copyRef,
+            this.changed
+          );
+          this.materializedRef = val;
+          return val;
+        };
+      }
+      default: {
+        const returnValue = Reflect.get(
+          this.copyRef ? this.copyRef.ref : this.originalTarget,
+          propKey
+        );
+
+        if (typeof returnValue === 'function') {
+          return (returnValue as Function).bind(receiver);
+        }
+        if (typeof returnValue !== 'object' || isMaterializable(returnValue)) {
+          return returnValue;
+        }
+        const proxyVal = this.createProxy(returnValue);
+        if (!this.copyRef) {
+          this.copyRef = makeCopyRef(this.originalTarget);
+        }
+        Reflect.set(this.copyRef.ref, propKey, proxyVal);
+        return proxyVal;
+      }
+    }
+  }
+  set(target: T, propKey: PropertyKey, value: any, receiver: any) {
+    if (this.materializedRef) {
+      throw Error('object proxy expired');
+    }
+    this.changed = true;
+    if (!this.copyRef) {
+      this.copyRef = makeCopyRef(this.originalTarget);
+    }
+    const ret = Reflect.set(this.copyRef.ref, propKey, value, this.copyRef.ref);
+    return ret;
+  }
+  deleteProperty(target: T, propKey: PropertyKey) {
+    if (this.materializedRef) {
+      throw Error('object proxy expired');
+    }
+    if (!this.copyRef) {
+      this.copyRef = makeCopyRef(this.originalTarget);
+    }
+    return Reflect.deleteProperty(this.copyRef.ref, propKey);
+  }
+  has(target: T, propKey: PropertyKey) {
+    switch (propKey) {
+      case MATERIALIZE_PROXY: {
+        return true;
+      }
+      default: {
+        return Reflect.has(this.copyRef ? this.copyRef.ref : target, propKey);
+      }
+    }
+  }
+  ownKeys(target: T) {
+    return Reflect.ownKeys(this.copyRef ? this.copyRef.ref : target);
+  }
+  getOwnPropertyDescriptor(target: T, propKey: PropertyKey) {
+    return Reflect.getOwnPropertyDescriptor(
+      this.copyRef ? this.copyRef.ref : target,
+      propKey
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { edit } from './immutable/edit';
 import util from 'util';
 import { pipeInto } from 'ts-functional-pipe';
-import { join, toArray } from 'ts-iterable-functions';
+import { join, toArray, range, map, forEach } from 'ts-iterable-functions';
+import { produce } from 'immer';
+import { performance } from 'perf_hooks';
 
 process.stdout.write('\u001b[3J\u001b[1J');
 console.clear();
@@ -13,7 +15,7 @@ try {
     arr: [{ n: 1 }, { n: 2 }, { n: 3 }],
   };
 
-  const res = edit(src1, (draft) => {
+  const editFunc = (draft: typeof src1) => {
     delete draft.a;
     draft.b.animal = 'giraffe';
     const arr = draft.arr;
@@ -28,25 +30,57 @@ try {
       nn.n = 999;
     }
     arr.push(...[9, 8, 7].map((v) => ({ n: v })));
-    //arr.copyWithin(0, 2, 4);
-    //arr.reverse();
+    arr.copyWithin(0, 2, 4);
+    arr.reverse();
     //console.log(Object.entries(draft));
-  });
-  console.log(util.inspect(res, undefined, 5, true));
-  console.log(util.inspect(src1, undefined, 5, true));
-  console.log(src1.b.animal === res.b.animal);
-  console.log(
-    pipeInto(
-      src1.arr,
-      join(
-        res.arr,
-        (a) => a,
-        (b) => b,
-        (a) => a
-      ),
-      toArray()
-    )
+  };
+  pipeInto(
+    range(0, 10),
+    map(() => edit(src1, editFunc)),
+    forEach(() => {})
   );
+  pipeInto(
+    range(0, 10),
+    map(() => produce(src1, editFunc)),
+    forEach(() => {})
+  );
+
+  const numRuns = 10000;
+  const run1Start = performance.now();
+  pipeInto(
+    range(0, numRuns),
+    map(() => edit(src1, editFunc)),
+    forEach(() => {})
+  );
+  const run1end = performance.now();
+
+   pipeInto(
+    range(0, numRuns),
+    map(() => produce(src1, editFunc)),
+    forEach(() => {})
+  );
+  const run2end = performance.now();
+
+  console.log(run1end - run1Start, run2end - run1end);
+
+  const res1 = edit(src1, editFunc);
+  console.log(util.inspect(res1, undefined, 5, true));
+  const res2 = produce(src1, editFunc);
+  console.log(util.inspect(res2, undefined, 5, true));
+  // console.log(util.inspect(src1, undefined, 5, true));
+  // console.log(src1.b.animal === res.b.animal);
+  // console.log(
+  //   pipeInto(
+  //     src1.arr,
+  //     join(
+  //       res.arr,
+  //       (a) => a,
+  //       (b) => b,
+  //       (a) => a
+  //     ),
+  //     toArray()
+  //   )
+  // );
   // console.log(res.b === src1.b);
   // console.log(res === src1);
 } catch (e: any) {
