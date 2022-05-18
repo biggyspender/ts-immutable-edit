12edc9bae7a48987eed443e292a4d258fcb42594
Chris Sperry <chris.sperry@outthink.io>
2022-05-18 23:18:45 +0000

    more perf tests

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,58 @@
 import { edit } from './immutable/edit';
 import util from 'util';
-import { produce } from 'immer';
+import { enableAllPlugins, produce, setAutoFreeze, setUseProxies } from 'immer';
 import { deepFreeze } from './immutable/deepFreeze';
 import { Mutable } from './immutable/types/Mutable';
 import { measureRunTime } from './measurement/measureRunTime';
 import { clearConsoleScrollback } from './util/clearConsoleScrollback';
+import { deepEqual } from 'ts-deep-equal';
 
 clearConsoleScrollback();
 console.clear();
+const MAX = 50000;
+const MODIFY_FACTOR = 0.1;
+
+enableAllPlugins();
+setUseProxies(true);
+setAutoFreeze(false);
 
 try {
+  test2();
+} catch (e: any) {
+  console.log(e.message, e.stack);
+}
+
+function test2() {
+  const baseState: {
+    todo: string;
+    done: boolean;
+    someThingCompletelyIrrelevant: number[];
+  }[] = [];
+  //console.log('Hh', MAX);
+  for (let i = 0; i < MAX; i++) {
+    baseState.push({
+      todo: 'todo_' + i,
+      done: false,
+      someThingCompletelyIrrelevant: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    });
+  }
+  //console.log(baseState);
+  const editFunc = (draft: Mutable<typeof baseState>) => {
+    for (let i = 0; i < MAX * MODIFY_FACTOR; i++) {
+      draft[i].done = true;
+    }
+  };
+  runTest(baseState, editFunc, 50, (v1, v2) =>
+    console.log(
+      deepEqual(v1, v2),
+      v1[0],
+      v1[0].someThingCompletelyIrrelevant ===
+        v2[0].someThingCompletelyIrrelevant
+    )
+  );
+}
+
+function test1() {
   const srcData = deepFreeze({
     a: { a: 1 } as { a: number } | undefined,
     b: { animal: 'monkey' },
@@ -35,8 +78,21 @@ try {
     arr.reverse();
     arr.length = 5;
   };
-  console.log('measuring performance...');
-  const numRuns = 20000;
+  runTest(srcData, editFunc, 20000, (res1, res2) => {
+    console.log(util.inspect(res1, undefined, 5, true));
+    console.log(util.inspect(res2, undefined, 5, true));
+    console.log(res1.arr[4], res1.arr[4] === res2.arr[4]);
+  });
+}
+
+function runTest<T extends object>(
+  srcData: T,
+  editFunc: (draft: Mutable<T>) => void,
+  numRuns: number,
+  inspectData?: (v1: T, v2: T) => void
+) {
+  console.log('measuring performance...', global.gc);
+  global.gc && global.gc();
   const editRunMeasurements = {
     name: 'edit',
     ...measureRunTime(numRuns, () => {
@@ -58,10 +114,6 @@ try {
   );
 
   const res1 = edit(srcData, editFunc);
-  console.log(util.inspect(res1, undefined, 5, true));
   const res2 = produce(srcData, editFunc);
-  console.log(util.inspect(res2, undefined, 5, true));
-  console.log(res1.arr[4], res1.arr[4] === res2.arr[4]);
-} catch (e: any) {
-  console.log(e.message, e.stack);
+  inspectData?.(res1, res2);
 }
