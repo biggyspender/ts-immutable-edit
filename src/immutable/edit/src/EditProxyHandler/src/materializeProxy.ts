71e9fb53c9191ba96525a4733df20755c6b20a63
Chris Sperry <chris.sperry@outthink.io>
2022-05-19 16:50:05 +0000

    refactor: fix typo - descendants vs descendents

--- src/immutable/edit/src/EditProxyHandler/src/materializeProxy.ts
+++ src/immutable/edit/src/EditProxyHandler/src/materializeProxy.ts
@@ -10,17 +10,17 @@ export function materializeProxy<T extends object>(
   if (!copyRef) {
     return { value: originalTarget, changed: false };
   }
-  let descendentsOrSelfChanged = changed;
+  let descendantsOrSelfChanged = changed;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const objRef: any = copyRef.ref;
   const propNames = Object.getOwnPropertyNames(objRef);
   for (const k of propNames) {
     const { value, changed } = materialize(objRef[k]);
-    descendentsOrSelfChanged = descendentsOrSelfChanged || changed;
+    descendantsOrSelfChanged = descendantsOrSelfChanged || changed;
     objRef[k] = value;
   }
   return {
-    value: descendentsOrSelfChanged ? copyRef.ref : originalTarget,
-    changed: descendentsOrSelfChanged,
+    value: descendantsOrSelfChanged ? copyRef.ref : originalTarget,
+    changed: descendantsOrSelfChanged,
   };
 }
