2c8fbb47e1164b171410349b69319f9e8927b9b5
Chris Sperry <chris.sperry@outthink.io>
2022-05-19 16:52:01 +0000

    feat(index.ts): export deepFreeze and Mutable/Immutable types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { edit } from "./immutable/edit";
+export { deepFreeze } from "./immutable/deepFreeze";
+export { Immutable } from "./immutable/types/Immutable";
+export { Mutable } from "./immutable/types/Mutable";
