24c0b4c83f2e1def53f4bbacf90700c2211542c2
Chris Sperry <chris.sperry@outthink.io>
2022-05-20 23:39:57 +0000

    fix(deepfreeze.ts): fix type error

--- src/immutable/deepFreeze.ts
+++ src/immutable/deepFreeze.ts
@@ -1,6 +1,6 @@
 import { Immutable } from "./types/Immutable";
 
-export function deepFreeze<T>(obj: T): Immutable<T> {
+export function deepFreeze<T extends object>(obj: T): Immutable<T> {
   const propNames = Object.getOwnPropertyNames(obj);
   for (const name of propNames) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
